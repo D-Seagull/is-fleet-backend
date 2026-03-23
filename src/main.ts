7f02335053cb96623a1c21b1ba1f76b784d66889
D-Seagull <dchaika.work@gmail.com>
2026-03-23 21:20:42 +0100

    added enableCors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      // 'https://is-fleet-frontend.vercel.app', // додаси пізніше
+    ],
+    credentials: true,
+  });
   app
     .useGlobalPipes(
       new ValidationPipe({
