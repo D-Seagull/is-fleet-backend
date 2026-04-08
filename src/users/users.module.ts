3d1c1b77c28f9736b5fbb0be87cdb05a3bfbdaa0
D-Seagull <dchaika.work@gmail.com>
2026-04-08 22:10:25 +0200

    added MailModule to users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [CloudinaryModule],
+  imports: [CloudinaryModule, MailModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
