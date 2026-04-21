64f4ccca921e0f011a78ffbc3f76a6fa0337f3b4
D-Seagull <dchaika.work@gmail.com>
2026-04-21 21:44:43 +0200

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://is-fleet-frontend.vercel.app'],
+    origin: ['https://is-fleet-frontend.vercel.app'],
     credentials: true,
   });
   app
