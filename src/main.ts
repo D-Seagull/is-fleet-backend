b5bacbe9c4eaf48c51e0e1076b3cdda08707a993
D-Seagull <dchaika.work@gmail.com>
2026-04-24 21:39:00 +0200

    edit cors again

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', process.env.FRONTEND_URL].filter(Boolean),
     credentials: true,
   });
   app
